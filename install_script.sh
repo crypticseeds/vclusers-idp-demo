f0d3147d219be755631da29d100644a7b8547fb0
Femi Akinlotan <94658643+crypticseeds@users.noreply.github.com>
2024-12-10 19:22:18 -0500

    Initial commit

---
+++ install_script.sh
@@ -0,0 +1,31 @@
+# Install ArgoCD
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+ 
+# Install Crossplane
+helm repo add crossplane-stable \
+    https://charts.crossplane.io/stable
+helm repo update
+helm upgrade --install crossplane crossplane-stable/crossplane \
+    --namespace crossplane-system --create-namespace --wait
+
+cd ./crossplane
+kubectl apply -f ./providers/
+kubectl wait --for=condition=healthy provider.pkg.crossplane.io \
+    --all --timeout=300s
+
+sleep 5
+
+kubectl apply -f ./provider-configs/
+kubectl apply -f xrds.yaml
+kubectl apply -f compositions.yaml
+
+# Install Backstage
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add backstage https://backstage.github.io/charts
+kubectl create ns backstage
+
+# Install Komoplane
+helm repo add komodorio https://helm-charts.komodor.io \
+  && helm repo update komodorio \
+  && helm upgrade --install komoplane komodorio/komoplane
